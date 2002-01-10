d3edba7cda73920f0af97135a0994f6f39a01074
engelhar <engelhar>
2002-01-10 21:01:57 +0000

    - Add comments for when to run autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# autogen.sh: Automatically generate the files necessary to build using
+# autoconf.  This must be run on a fresh checkout from CVS and also if any
+# autoconf files (configure.in, etc.) are updated.
 aclocal
 libtoolize --force
 autoheader
