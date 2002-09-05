aa0e62852cc5f151b58dfb3f89f91165e0dc07e5
jheffner <jheffner>
2002-09-05 20:02:48 +0000

    Add proper ipv6 support.

--- util/scripts/readall.c
+++ util/scripts/readall.c
@@ -11,7 +11,7 @@
  * collaborate with all of the users.  So for the time being, please refer
  * potential users to us instead of redistributing web100.
  *
- * $Id: readall.c,v 1.1 2002/01/14 17:50:30 jestabro Exp $
+ * $Id: readall.c,v 1.2 2002/09/05 20:02:48 jheffner Exp $
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,84 +19,130 @@
 #include "web100.h"
 
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	web100_agent* agent;
-	web100_group* group;
+    web100_agent *agent;
+    web100_group *group;
+    web100_group *read_grp;
+    web100_var *addr_type, *laddr, *raddr, *lport, *rport;
 
-	if ((agent = web100_attach(WEB100_AGENT_TYPE_LOCAL, NULL)) == NULL) {
-		web100_perror("web100_attach");
+    if ((agent = web100_attach(WEB100_AGENT_TYPE_LOCAL, NULL)) == NULL) {
+        web100_perror("web100_attach");
         exit(EXIT_FAILURE);
-	}
-	
-	group = web100_group_head(agent);
+    }
+    
+    if ((read_grp = web100_group_find(agent, "read")) == NULL) {
+        web100_perror("web100_group_find: read");
+        exit(EXIT_FAILURE);
+    }
+    if ((addr_type = web100_var_find(read_grp, "LocalAddressType")) == NULL) {
+        web100_perror("web100_var_find: LocalAddressType");
+        exit(EXIT_FAILURE);
+    }
+    if ((laddr = web100_var_find(read_grp, "LocalAddress")) == NULL) {
+        web100_perror("web100_var_find: LocalAddress");
+        exit(EXIT_FAILURE);
+    }
+    if ((raddr = web100_var_find(read_grp, "RemoteAddress")) == NULL) {
+        web100_perror("web100_var_find: RemoteAddress");
+        exit(EXIT_FAILURE);
+    }
+    if ((lport = web100_var_find(read_grp, "LocalPort")) == NULL) {
+        web100_perror("web100_var_find: LocalPort");
+        exit(EXIT_FAILURE);
+    }
+    if ((rport = web100_var_find(read_grp, "RemotePort")) == NULL) {
+        web100_perror("web100_var_find: RemotePort");
+        exit(EXIT_FAILURE);
+    }
+    
+    group = web100_group_head(agent);
+    
+    while (group) {
+        web100_connection *conn;
 
-	while (group) {
-        web100_connection* conn;
-        
-		printf("Group \"%s\"\n", web100_get_group_name(group));
+        printf("Group \"%s\"\n", web100_get_group_name(group));
 
         conn = web100_connection_head(agent);
 
-		while (conn) {
-            web100_var* var;
-            web100_snapshot* snap;
-            struct web100_connection_spec spec;
+        while (conn) {
+            web100_var *var;
+            web100_snapshot *snap;
+            int type;
             int cid;
-            char buf[8];
+            char buf[256];
 
-			cid = web100_get_connection_cid(conn);
-			web100_get_connection_spec(conn, &spec);
-			{
-				unsigned char *src = (unsigned char *)&spec.src_addr;
-				unsigned char *dst = (unsigned char *)&spec.dst_addr;
-				printf("Connection %d (%u.%u.%u.%u:%u %u.%u.%u.%u:%u)\n",
-				       cid,
-				       src[0], src[1], src[2], src[3], spec.src_port,
-				       dst[0], dst[1], dst[2], dst[3], spec.dst_port);
-			}
-			
-			if ((snap = web100_snapshot_alloc(group, conn)) == NULL) {
-				web100_perror("web100_snapshot_alloc");
-                exit(EXIT_FAILURE);
-			}
+            cid = web100_get_connection_cid(conn);
+            printf("Connection %d (", cid);
             
-			if (web100_snap(snap)) {
-				perror("web100_snap");
-				if (web100_errno == WEB100_ERR_NOCONNECTION)
-					continue;
+            if (web100_raw_read(addr_type, conn, buf) != WEB100_ERR_SUCCESS) {
+                web100_perror("web100_raw_read");
                 exit(EXIT_FAILURE);
-			}
+            }
+            type = *(int *)buf;
+            type = (type == WEB100_ADDRTYPE_IPV4 ? WEB100_TYPE_INET_ADDRESS_IPV4 : WEB100_TYPE_INET_ADDRESS_IPV6);
+            if (web100_raw_read(laddr, conn, buf) != WEB100_ERR_SUCCESS) {
+                web100_perror("web100_raw_read");
+                exit(EXIT_FAILURE);
+            }
+            printf("%s ", web100_value_to_text(type, buf));
+            if (web100_raw_read(lport, conn, buf) != WEB100_ERR_SUCCESS) {
+                web100_perror("web100_raw_read");
+                exit(EXIT_FAILURE);
+            }
+            printf("%s  ", web100_value_to_text(WEB100_TYPE_INET_PORT_NUMBER, buf));
+            if (web100_raw_read(raddr, conn, buf) != WEB100_ERR_SUCCESS) {
+                web100_perror("web100_raw_read");
+                exit(EXIT_FAILURE);
+            }
+            printf("%s ", web100_value_to_text(type, buf));
+            if (web100_raw_read(rport, conn, buf) != WEB100_ERR_SUCCESS) {
+                web100_perror("web100_raw_read");
+                exit(EXIT_FAILURE);
+            }
+            printf("%s)\n", web100_value_to_text(WEB100_TYPE_INET_PORT_NUMBER, buf));
+
+            if ((snap = web100_snapshot_alloc(group, conn)) == NULL) {
+                web100_perror("web100_snapshot_alloc");
+                exit(EXIT_FAILURE);
+            }
+
+            if (web100_snap(snap)) {
+                perror("web100_snap");
+                if (web100_errno == WEB100_ERR_NOCONNECTION)
+                    continue;
+                exit(EXIT_FAILURE);
+            }
 
             var = web100_var_head(group);
 
-			while (var) {
-				if (web100_snap_read(var, snap, buf)) {
-					web100_perror("web100_snap_read");
+            while (var) {
+                if (web100_snap_read(var, snap, buf)) {
+                    web100_perror("web100_snap_read");
                     exit(EXIT_FAILURE);
-				}
-				
-				printf("%-20s %s\n",
+                }
+
+                printf("%-20s %s\n",
                        web100_get_var_name(var),
-                       web100_value_to_text(web100_get_var_type(var), buf));
+                       web100_value_to_text(web100_get_var_type(var),
+                                            buf));
 
-				var = web100_var_next(var);
-			}
-			
-			web100_snapshot_free(snap);
+                var = web100_var_next(var);
+            }
 
-			conn = web100_connection_next(conn);
+            web100_snapshot_free(snap);
 
-			if (conn)
+            conn = web100_connection_next(conn);
+
+            if (conn)
                 printf("\n");
-		}
+        }
 
         group = web100_group_next(group);
 
         if (group)
             printf("\n");
-	}
-	
-	return 0;
+    }
+
+    return 0;
 }
