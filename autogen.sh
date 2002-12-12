643d6026116132a6b0ae7fd97295a042c0289fe3
engelhar <engelhar>
2002-12-12 17:43:59 +0000

    - Make userland build on Mac OS X, not that it is very useful yet...

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,22 @@
 # autoconf.  This must be run on a fresh checkout from CVS and also if any
 # autoconf files (configure.in, etc.) are updated.
 
-aclocal -I m4 || exit 1
-libtoolize --force || exit 1
+for i in m4 /usr/local/share/aclocal; do
+    if [ -d $i ]; then
+        ACLOCAL_OPTS="$ACLOCAL_OPTS -I $i"
+    fi
+done
+aclocal $ACLOCAL_OPTS || exit 1
+
 autoheader || exit 1
-automake --add-missing --include-deps --foreign || exit 1
 autoconf || exit 1
+
+# Handle Mac OS X's non-standard naming of libtoolize
+for i in libtoolize glibtoolize; do
+    if which $i > /dev/null; then
+        LIBTOOLIZE=$i
+    fi
+done
+$LIBTOOLIZE --automake || exit 1
+
+automake --add-missing --include-deps --foreign || exit 1
